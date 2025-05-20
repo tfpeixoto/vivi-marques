da20970289e558877abe440fabb414812d8f4b8c
Thiago Peixoto <tfpeixoto@gmail.com>
2025-05-20 00:01:09 -0300

    posts development

--- wp-content/themes/vivi-marques/single.php
+++ wp-content/themes/vivi-marques/single.php
@@ -1,8 +1,9 @@
 <?php require_once('parts/header.php'); ?>
 
 <section class="hero-post">
-  <div class="container">
+  <div class="container hero-post__container">
     <div class="hero-post__content">
+      <span class="hero-post__breadcrumb"><a href="<?= home_url(); ?>">Home</a> | <?php the_title(); ?></span>
       <h1><?php the_title(); ?></h1>
       <?php the_excerpt(); ?>
     </div>
@@ -11,7 +12,7 @@
       <?php if (has_post_thumbnail()) : ?>
         <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
       <?php else : ?>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/default-image.jpg" alt="<?php the_title(); ?>" class="img-fluid">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/thumb-post-fallback.png" alt="<?php the_title(); ?>" class="img-fluid">
       <?php endif; ?>
     </div>
   </div>
@@ -27,7 +28,7 @@
 
 <section class="comments">
   <div class="container">
-    Comentários
+    <?php comments_template(); ?>
   </div>
 </section>
 
